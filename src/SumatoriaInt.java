4c7c0c7d54f6d9911f9ae3b8403a9c0b76f87369
MauricioResmar <mauricioremar@gmail.com>
2022-08-05 12:37:06 -0500

    Se agrega el proyecto

---
+++ src/SumatoriaInt.java
@@ -0,0 +1,18 @@
+public class SumatoriaInt
+        {
+         public static void main(String[] args)
+            {
+
+            int num1=3;
+            int num2=10;
+            int num3=5;
+
+            int resultado = sumatoria(num1,num2,num3);
+            System.out.println(resultado);
+            }
+                public static int sumatoria (int a, int b, int c)
+                {
+                    int resultado = a+b+c;
+                    return resultado;
+                }
+        }
